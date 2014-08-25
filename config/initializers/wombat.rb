38a159b05f929ffccbb3de5f7f672481829d0122
Ben Fitzgerald <theeuclidean@gmail.com>
2014-08-25 17:31:48 +0200

    Adding Address emission

--- config/initializers/wombat.rb
+++ config/initializers/wombat.rb
@@ -4,10 +4,11 @@ Spree::Wombat::Config.configure do |config|
   config.queue_prefix = 'ProjectJ::'
   config.connection_token = 'YOUR TOKEN'
   config.connection_id = 'YOUR CONNECTION ID'
-  config.push_objects = %w(Spree::Order Spree::Product Spree::Variant)
+  config.push_objects = %w(Spree::Order Spree::Product Spree::Variant Spree::Address)
   config.payload_builder = {
     'Spree::Order' => {:serializer => 'Spree::Wombat::OrderSerializer', :root => 'orders'},
     'Spree::Product' => {:serializer => 'Spree::Wombat::ProductSerializer', :root => 'products'},
     'Spree::Variant' => {:serializer => 'Spree::Wombat::VariantSerializer', :root => 'variants'},
+    'Spree::Address' => {:serializer => 'Spree::Wombat::AddressSerializer', :root => 'addresses'},
   }
 end
