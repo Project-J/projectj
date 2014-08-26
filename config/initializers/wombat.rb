e023a42dfcadc71aaf2d46b0454cfe827b868cc2
Ben Fitzgerald <theeuclidean@gmail.com>
2014-08-26 19:42:05 +0200

    Adding emission for users

--- config/initializers/wombat.rb
+++ config/initializers/wombat.rb
@@ -4,11 +4,12 @@ Spree::Wombat::Config.configure do |config|
   config.queue_prefix = 'ProjectJ::'
   config.connection_token = 'YOUR TOKEN'
   config.connection_id = 'YOUR CONNECTION ID'
-  config.push_objects = %w(Spree::Order Spree::Product Spree::Variant Spree::Address)
+  config.push_objects = %w(Spree::Order Spree::Product Spree::Variant Spree::Address Spree::User)
   config.payload_builder = {
     'Spree::Order' => {:serializer => 'Spree::Wombat::OrderSerializer', :root => 'orders'},
     'Spree::Product' => {:serializer => 'Spree::Wombat::ProductSerializer', :root => 'products'},
     'Spree::Variant' => {:serializer => 'Spree::Wombat::VariantSerializer', :root => 'variants'},
     'Spree::Address' => {:serializer => 'Spree::Wombat::AddressSerializer', :root => 'addresses'},
+    'Spree::User' => {:serializer => 'Spree::Wombat::UserSerializer', :root => 'users'},
   }
 end
